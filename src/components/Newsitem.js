c072dbb5c413ed6cb02a4a4129277bde64807968
mayureshraval <mayureshraval2012@gmail.com>
2023-02-28 22:36:40 +0530

    new commit

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-    let {title,description,imgurl,newsurl} = this.props;
+    let {title,description,imageUrl,newsurl} = this.props;
     return (
         <>
            <div className="card" style={{width: '18rem'}}>
-            <img src={imgurl} style={{width:'100%',height:'150px'}} className="card-img-top" alt="..." />
+            <img src={imageUrl} style={{width:'100%',height:'150px'}} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
